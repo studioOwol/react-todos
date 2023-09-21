ad20a08757106d3abfadbe5acf8a51acf6aac4e6
studioOwol <from.owol@gmail.com>
2023-09-21 16:07:37 +0900

    feat: add checkbox list

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import CssBaseline from "@mui/material/CssBaseline";
+import "./App.css";
+import TodoList from "./TodoList";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+      <TodoList />
+    </>
+  );
+}
+
+export default App;
