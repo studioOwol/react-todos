2d963873f1ed319cd0fe5fb9c146f17d352914e9
Nahyun <from.owol@gmail.com>
2023-09-21 17:30:45 +0900

    feat: add delete function on the checkbox

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -11,10 +11,16 @@ const initialTodos = [
 
 export default function TodoList() {
   const [todos, setTodos] = useState(initialTodos);
+  const removeTodo = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((t) => t.id !== id);
+    });
+  };
+
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
       {todos.map((todo) => (
-        <TodoItem todo={todo} key={todo.id} />
+        <TodoItem todo={todo} key={todo.id} remove={removeTodo} />
       ))}
     </List>
   );
