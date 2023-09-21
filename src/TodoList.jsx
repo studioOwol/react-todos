b7bfdd104e73a952434f045c20fd4eb3bda8ca03
Nahyun <from.owol@gmail.com>
2023-09-21 20:36:51 +0900

    feat: add new list item form

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,6 +1,7 @@
 import List from "@mui/material/List";
 import TodoItem from "./TodoItem";
 import { useState } from "react";
+import TodoForm from "./TodoForm";
 
 const initialTodos = [
   { id: 1, text: "walk the dog", completed: false },
@@ -27,6 +28,11 @@ export default function TodoList() {
       });
     });
   };
+  const addTodo = (text) => {
+    setTodos((prevTodos) => {
+      return [...prevTodos, { text: text, id: 8, completed: false }];
+    });
+  };
 
   return (
     <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
@@ -38,6 +44,7 @@ export default function TodoList() {
           toggle={() => toggleTodo(todo.id)}
         />
       ))}
+      <TodoForm addTodo={addTodo} />
     </List>
   );
 }
