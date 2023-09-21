23c84c60c426a3352e5c8a1779e671908b60ebc4
Nahyun <from.owol@gmail.com>
2023-09-21 20:08:02 +0900

    feat: add toggle function

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -6,7 +6,7 @@ import Checkbox from "@mui/material/Checkbox";
 import IconButton from "@mui/material/IconButton";
 import CommentIcon from "@mui/icons-material/Comment";
 
-export default function TodoItem({ todo, remove }) {
+export default function TodoItem({ todo, remove, toggle }) {
   const labelId = `checkbox-list-label-${todo.id}`;
   const removeTodo = () => {
     remove(todo.id);
@@ -29,6 +29,7 @@ export default function TodoItem({ todo, remove }) {
             tabIndex={-1}
             disableRipple
             inputProps={{ "aria-labelledby": labelId }}
+            onChange={toggle}
           />
         </ListItemIcon>
         <ListItemText id={labelId} primary={todo.text} />
