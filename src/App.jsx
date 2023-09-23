335200722d7877d4cd6f11a610a07a5a2da86087
Nahyun <from.owol@gmail.com>
2023-09-23 20:36:00 +0900

    feat: add a navbar css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import "./App.css";
 import TodoList from "./TodoList";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <>
       <CssBaseline />
-      <h1>Todos</h1>
+      <Navbar />
+
       <TodoList />
     </>
   );
